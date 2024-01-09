e98bc0e3c4b88f70926347f264d189093578f992
Clément Guiton <clement.guiton.dev@gmail.com>
2024-01-09 12:46:59 +0100

    add caching and query all pokémon

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -2,16 +2,43 @@ import 'dart:convert';
 import 'package:pokedex/pokemon.dart';
 import 'package:http/http.dart' as http;
 
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+
 class ApiService {
+  final cache = DefaultCacheManager();
+  final cacheKey = 'api_response.json';
+  List<Pokemon> pokemonList = [];
+
   final String baseUrl = 'https://pokeapi.co/api/v2';
 
   Future<List<Pokemon>> fetchPokemonList() async {
-    final response = await http.get(Uri.parse('$baseUrl/pokemon?limit=20'));
+    final response = await http.get(Uri.parse('$baseUrl/pokemon?limit=10000'));
     if (response.statusCode == 200) {
-      final List<dynamic> data = jsonDecode(response.body)['results'];
-      return data.map((json) => Pokemon.fromJson(json)).toList();
+      return decode(response.body);
     } else {
       throw Exception('Failed to load Pokemon list');
     }
   }
+
+  // get list of pokemon from cache if available
+  Future<List<Pokemon>> getPokemonList() async {
+    if (pokemonList.isNotEmpty) {
+      return pokemonList;
+    }
+
+    final file = await cache.getSingleFile(cacheKey);
+    if (file != null && file.existsSync()) {
+      final jsonData = await file.readAsString();
+      pokemonList = decode(jsonData);
+    } else {
+      pokemonList = await fetchPokemonList();
+      await cache.putFile(cacheKey, utf8.encode(jsonEncode(pokemonList)));
+    }
+    return pokemonList;
+  }
+}
+
+List<Pokemon> decode(String jsonData) {
+  final List<dynamic> data = jsonDecode(jsonData)['results'];
+  return data.map((json) => Pokemon.fromJson(json)).toList();
 }
